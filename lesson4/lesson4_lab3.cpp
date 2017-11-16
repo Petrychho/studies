947c10c72a9ac21cfa74a78b54c6241b35259ded
Petr <32482054+Petrychho@users.noreply.github.com>
2017-11-16 12:53:09 +0300

    Update lesson4_lab3.cpp

--- lesson4/lesson4_lab3.cpp
+++ lesson4/lesson4_lab3.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 
-void swap(int *a, int size_a, int* b, int size_b)
+void swap(int *a, int *size_a, int *b, int *size_b, int *out_mass)
 {
-    int* out_mass = new int[size_a + size_b];
-
-    while(*a != size_a || *b != size_b)
+    while(*a != *size_a || *b != *size_b)
     {
     	*out_mass++ = (*a <= *b) ? *a++ : *b++;
     }
-    while(*a != size_a)
+    while(*a != *size_a)
     {
     	*out_mass++ = *a++; 
     }
-    while(*b != size_b)
+    while(*b != *size_b)
     {
     	*out_mass++ = *b++;
-    }
-    
-    for (int i = 0; i < size_a + size_b; ++i)
-    {
-        std::cout << out_mass[i] << ' ';
     }
 }
 
@@ -27,6 +20,11 @@ int main()
 {
     int a[10] = {1,2,3,4,5,6,7,8,9,10};
     int b[10] = {0,6,7,8,9,10,11,12,13,14};
+    int out_mass[20];
 
-    swap(a, sizeof(a)/sizeof(a[0]), b, sizeof(b)/sizeof(b[0]));
+    swap(a, a + sizeof(a)/sizeof(a[0]), b, b + sizeof(b)/sizeof(b[0]), out_mass);
+    for( const int &x : out_mass)
+    {
+    	std::cout << x << " ";
+    }
 }
